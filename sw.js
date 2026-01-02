b661e14735f6fc9e16cf833a181dfa702772ab33
MonoLN2 <138714452+MonoLN2@users.noreply.github.com>
2026-01-02 10:33:47 -0500

    Refactor service worker event listeners
    Removed fetch event listener and added message event listener for skipWaiting action.

--- sw.js
+++ sw.js
@@ -35,10 +35,8 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('message', (event) => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
